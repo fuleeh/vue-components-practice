283f49caeb1e7fbd23dd1ffbf61b0872446d90ca
Fülöp Szilárd <fszili882@gmail.com>
2022-02-07 21:04:03 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import { createApp } from 'vue';
 
-createApp({}).mount('#app');
+import App from './App';
+import BaseCard from './components/UI/BaseCard';
+import BaseButton from './components/UI/BaseButton';
+import BaseDialog from './components/UI/BaseDialog';
+
+const app = createApp(App);
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
+
+app.mount('#app');
